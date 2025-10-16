070a6b8e2bbbe400491ad775708355044571676c
Kevin-2007-56 <Mysad224@gmail.com>
2025-10-16 20:37:38 +0700

    Add files via upload

--- server.js
+++ server.js
@@ -4,21 +4,16 @@ const axios = require('axios');
 const cors = require('cors');
 
 const app = express();
-// เราจะใช้ Port ที่ Render กำหนดให้ หรือใช้ 4000 ถ้าทดสอบบนเครื่อง
 const port = process.env.PORT || 4000; 
-// นี่คือส่วนที่สำคัญที่สุดที่เราเพิ่มเข้ามาตามเอกสาร
 const host = '0.0.0.0'; 
 
-// Middleware
 app.use(cors());
 app.use(express.json());
 
-// Health Check Route (ประตูหน้าสำหรับ Render)
 app.get('/', (req, res) => {
   res.status(200).send('<h1>Backend Server is RUNNING CORRECTLY!</h1>');
 });
 
-// API Route (ประตูหลังสำหรับดึงข้อมูลเกม)
 app.post('/api/games', async (req, res) => {
   const BARBAR_API_KEY = "BGZGP1760538243999";
   try {
@@ -33,7 +28,6 @@ app.post('/api/games', async (req, res) => {
   }
 });
 
-// เราต้องบอกให้ .listen ทำงานบน host ที่ถูกต้องด้วย
 app.listen(port, host, () => {
   console.log(`Server is listening on http://${host}:${port}`);
 });
